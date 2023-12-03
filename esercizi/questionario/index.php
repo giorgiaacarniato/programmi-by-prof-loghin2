8d99a92be439bfae455ee96ac43a0dc24865ea17
giorgiaacarniato <146830965+giorgiaacarniato@users.noreply.github.com>
2023-12-03 20:47:48 +0000

    mmm

--- esercizi/questionario/index.php
+++ esercizi/questionario/index.php
@@ -2,41 +2,38 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registrazione Utente</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
 <body class="text-center">
-    
-<main class="form-signin">
+    <main class="form-signin">
+        <h1>Benvenuto utente nel nostro sito, per favore inserisci i tuoi dati!</h1>
+        <div class="form-floating">
+            <form action="risultato.php" method="GET">
+                <input type="text" class="form-control" id="fname" name="fnome" placeholder="Nome" required>
+                <label for="fname"></label>
 
-    <h1> Benvenuto utente nel nostro sito, per favore inserisci i tuoi dato! </h1>
+                <input type="text" class="form-control" id="lname" name="lnome" placeholder="Cognome" required>
+                <label for="lname"></label>
 
-   <div class="form-floating">
-   <form action="./risultato.php">
-      <input type="text" class="form-control" id="fname" placeholder="Nome">
-      <label for="fname "></label>
-      <input type="text" class="form-control" id="lname" placeholder="Cognome">
-      <label for="lname"></label>
-      <input type="number" class="form-control" id="età" placeholder="Età">
-      <label for="età"></label>
-      <input type="text" class="form-control" id="sport" placeholder="Sport Preferito">
-      <label for="sport"></label>
-      <input type="color" class="form-control" id="colore" placeholder="Scegli un colore">
-      <label for="colore"></label>
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword"></label>
+                <input type="number" class="form-control" id="età" name="age" placeholder="Età" required>
+                <label for="età"></label>
 
-      <button class="w-100 btn btn-lg btn-primary" type="submit"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Registrazione</font></font></button>
+                <input type="text" class="form-control" id="sport" name="sport" placeholder="Sport Preferito" required>
+                <label for="sport"></label>
 
-   </form>
-   </div>
-      
-  </form>
+                <input type="color" class="form-control" id="colore" name="color" placeholder="Scegli un colore" required>
+                <label for="colore"></label>
 
+                <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password" required>
+                <label for="floatingPassword"></label>
 
-
-</main>
-
+                <button class="w-100 btn btn-lg btn-primary" type="submit">Registrazione</button>
+            </form>
+        </div>
+    </main>
 </body>
-
-
-
-
+</html>
--- esercizi/questionario/risultato.php
+++ esercizi/questionario/risultato.php
@@ -1,5 +1,43 @@
+<!DOCTYPE html>
 <html>
-    <body>
-       <p> mucca </p>
-    </body>
+<head>
+    <title>Risultato Registrazione</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        body {
+            font-size: 1.2em;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            
+        }
+
+        .result-container {
+            text-align: center;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="result-container">
+        <?php
+        if(!empty($_GET)){
+            $fnome = $_GET["fnome"];
+            $lnome = $_GET["lnome"];
+            $age = $_GET["age"];
+            $sport = $_GET["sport"];
+            $color = $_GET["color"];
+
+            echo "<div class='bg-light p-4' style='background-color: $color;'>";
+            echo "<p><strong>Nome:</strong> $fnome </p>";
+            echo "<p><strong>Cognome:</strong> $lnome </p>";
+            echo "<p><strong>Età:</strong> $age </p>";
+            echo "<p><strong>Sport:</strong> $sport </p>";
+            echo "</div>";
+        }
+        ?>
+    </div>
+</body>
 </html>
+
