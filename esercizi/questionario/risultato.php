0d35350f30cdf533990831dbab8fdb100e688c2e
giorgiaacarniato <146830965+giorgiaacarniato@users.noreply.github.com>
2023-12-07 16:36:38 +0000

    mm

--- esercizi/questionario/risultato.php
+++ esercizi/questionario/risultato.php
@@ -29,32 +29,27 @@ echo "<div class='risultato' style='background-color: $color;'>";
 ?>
 
 <table>
-    <th>Nome</th>
-    <th>Cognome</th>
-    <th>Età</th>
-    <th>Sport preferito</th>
-
-    <tr><td>
-        <?php
+<tr><td>Nome</td><td>  <?php
         $fnome = $_GET["fnome"];
         echo $fnome;
         ?>
-    </td><td>
-    <?php
+</td></tr>
+<tr><td> Cognome </td><td>  <?php
         $lnome = $_GET["lnome"];
         echo $lnome;
         ?>
-    </td><td>
-    <?php
-        $age = $_GET["age"];
-        echo $age;
+</td></tr>
+<tr><td>Età</td><td>  <?php
+         $age = $_GET["age"];
+         echo $age;
         ?>
-    </td><td>
-    <?php
-        $sport = $_GET["sport"];
-        echo $sport;
+</td></tr>
+<tr><td>Sport preferito</td><td>  <?php
+       $sport = $_GET["sport"];
+       echo $sport;
         ?>
-    </td>
+ </td></tr>
+
 </table>
 
 
